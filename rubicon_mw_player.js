97d29d0436d4ba9818a59930a2b272a8afb575e5
Adrian Ruiz <adrianruiz@Adrians-MacBook-Air-2.local>
2017-02-15 16:51:23 -0800

    rubicon test page

---
+++ rubicon_mw_player.js
@@ -0,0 +1,51 @@
+rubicon_function()
+   {
+     var lkqdSettings = {
+       pid: 282,
+       sid: 183601,
+       playerContainerId: '',
+       playerId: '',
+       playerWidth: '',
+       playerHeight: '',
+       execution: 'interstitial',
+       placement: '',
+       playInitiation: 'user',
+       volume: 100,
+       trackImp: '',
+       trackClick: '',
+       custom1: '',
+       custom2: '',
+       custom3: '',
+       pubMacros: '',
+       dfp: false,
+       lkqdId: new Date().getTime().toString() + Math.round(Math.random()*1000000000).toString(),
+     };
+
+     var lkqdVPAID;
+     var creativeData = '';
+     var environmentVars = { slot: document.getElementById(lkqdSettings.playerContainerId), videoSlot: document.getElementById(lkqdSettings.playerId), videoSlotCanAutoPlay: true, lkqdSettings: lkqdSettings };
+
+     function onVPAIDLoad()
+     {
+       lkqdVPAID.subscribe(function() { lkqdVPAID.startAd(); }, 'AdLoaded');
+     }
+
+     var vpaidFrame = document.createElement('iframe');
+     vpaidFrame.id = lkqdSettings.lkqdId;
+     vpaidFrame.name = lkqdSettings.lkqdId;
+     vpaidFrame.style.display = 'none';
+     var vpaidFrameLoaded = function() {
+       vpaidLoader = vpaidFrame.contentWindow.document.createElement('script');
+       vpaidLoader.src = 'https://ad.lkqd.net/vpaid/formats.js?pid=282&sid=183601';
+       vpaidLoader.onload = function() {
+         lkqdVPAID = vpaidFrame.contentWindow.getVPAIDAd();
+         onVPAIDLoad();
+         lkqdVPAID.handshakeVersion('2.0');
+         lkqdVPAID.initAd(lkqdSettings.playerWidth, lkqdSettings.playerHeight, 'normal', 600, creativeData, environmentVars);
+       };
+       vpaidFrame.contentWindow.document.body.appendChild(vpaidLoader);
+     };
+     vpaidFrame.onload = vpaidFrameLoaded;
+     vpaidFrame.onerror = vpaidFrameLoaded;
+     document.documentElement.appendChild(vpaidFrame);
+   }();

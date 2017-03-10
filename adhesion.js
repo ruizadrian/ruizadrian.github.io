f1f72e924b1b1458ef9c1d571b8ab618721aa13c
Adrian Ruiz <adrianruiz@Adrians-MacBook-Air-2.local>
2017-03-10 03:32:00 -0800

    adhesion upadte

--- adhesion.js
+++ adhesion.js
@@ -21,12 +21,12 @@ else {
         el.style.zIndex = "100";
 				var closeButton = document.getElementById("HCM");
 				closeButton.innerHTML = "Xzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
-				closeButton.style.position = "absolute";
-				closeButton.style.width = "15px";
-				closeButton.style.height = "15px";
-				closeButton.style.top = "0px";
-				closeButton.style.right = "50px";
-				closeButton.style.zIndex = "500";
+				// closeButton.style.position = "absolute";
+				// closeButton.style.width = "15px";
+				// closeButton.style.height = "15px";
+				// closeButton.style.top = "0px";
+				// closeButton.style.right = "50px";
+				// closeButton.style.zIndex = "500";
 
 				//closeButton.style.backgroundImage = "url('http://www.hcodetestpage.com/close.png')";
 				//closeButton.style.backgroundRepeat = "no-repeat";

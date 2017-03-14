e5190f3209fc5848e31dcc25997c5d00aa7e56d1
Adrian Ruiz <adrianruiz@Adrians-MacBook-Air-2.local>
2017-03-13 18:25:10 -0700

    spotx outstream

--- spotx_outstream.js
+++ spotx_outstream.js
@@ -27,16 +27,16 @@ else {
 			tag_id = el.id;
 			if( tag_id.indexOf("div-gpt-ad") >= 0){
         //alert(el.id);
-        <script type="text/javascript">
-            function myAdDoneFunction(spotx_ad_found)  {
-                if(spotx_ad_found)  {
-                    // code to track ad playing here
-                }
-                else {
-                    // code to place backup ad request here
-                }
-            }
-        </script>
+        // <script type="text/javascript">
+        //     function myAdDoneFunction(spotx_ad_found)  {
+        //         if(spotx_ad_found)  {
+        //             // code to track ad playing here
+        //         }
+        //         else {
+        //             // code to place backup ad request here
+        //         }
+        //     }
+        // </script>
         <script type="text/javascript" src="//js.spotx.tv/easi/v1/177926.js" data-spotx_channel_id="177926" data-spotx_ad_unit="incontent" data-spotx_ad_done_function="myAdDoneFunction" data-spotx_content_width="450" data-spotx_content_height="370" data-spotx_https="0" data-spotx_embed_element_id=el.id></script>
       }
     }

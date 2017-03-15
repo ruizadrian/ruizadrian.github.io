240a02ec9127c3ef4333d8fad221558ef678d1f4
Adrian Ruiz <adrianruiz@Adrians-MacBook-Air-2.local>
2017-03-15 00:13:06 -0700

    test pages

--- adhesion.js
+++ adhesion.js
@@ -2,17 +2,7 @@ function updateParentDiv() {
 	var tag_id;
 	var el;
 	var closeButton;
-	closeButton = document.getElementById("close");
-	closeButton.style.position = 'fixed';
-	closeButton.style.top = "0px";
-	closeButton.style.right = "10px";
-	closeButton.style.zIndex = "1000";
-	closeButton.style.backgroundImage = "url('https://s3.amazonaws.com/script-tags/close.png')";
-	closeButton.style.backgroundSize = 'contain';
-	closeButton.style.backgroundRepeat = 'no-repeat';
-	closeButton.style.width = "18px";
-	closeButton.style.height = "18px";
-	closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false; console.log("closed the ad");};
+
 if ( window.self != window.top) {
     el = window.parent.document.getElementsByName(window.self.name) [0];
 
@@ -45,7 +35,18 @@ else {
 					//el.style.padding = "10px 0px 0px 0px"
 			    el.style.height = "50px";
 			    el.style.width = "320px";
-
+					
+					closeButton = document.getElementById("close");
+					closeButton.style.position = 'fixed';
+					closeButton.style.top = "0px";
+					closeButton.style.right = "10px";
+					closeButton.style.zIndex = "1000";
+					closeButton.style.backgroundImage = "url('https://s3.amazonaws.com/script-tags/close.png')";
+					closeButton.style.backgroundSize = 'contain';
+					closeButton.style.backgroundRepeat = 'no-repeat';
+					closeButton.style.width = "18px";
+					closeButton.style.height = "18px";
+					closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false; console.log("closed the ad");};
 
 			    // closeButton = document.getElementById("close");
 			    // closeButton.style.position = 'fixed';

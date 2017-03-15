109e1b1bbe405a84923a8b068845e28bf188c5c1
Adrian Ruiz <adrianruiz@Adrians-MacBook-Air-2.local>
2017-03-15 09:28:46 -0700

    test pages

--- adhesion.js
+++ adhesion.js
@@ -2,19 +2,19 @@ function updateParentDiv() {
 	var tag_id;
 	var el;
 	var closeButton;
-
+  var innerDiv;
 if ( window.self != window.top) {
     el = window.parent.document.getElementsByName(window.self.name) [0];
 
     // Reference iFrame
     el.height = "60px";
-    el.width = "320px";
-    // el.style.zIndex = "900";
+    el.width = "340px";
+    el.style.zIndex = "900";
 
 
     // Reference iFrame
-    window.self.document.getElementsByTagName("body")[0].style.margin="10px 0px 0px 0px";
-    // window.self.document.getElementsByTagName("body")[0].style.padding="0px";
+    window.self.document.getElementsByTagName("body")[0].style.margin="10px 10px 0px 10px";
+    window.self.document.getElementsByTagName("body")[0].style.padding="0px";
 
 }
 else {
@@ -22,7 +22,6 @@ else {
 }
 	while (el.parentNode) {
 		el = el.parentNode;
-
 		if (el.tagName == 'DIV') {
 			tag_id = el.id;
 			if( tag_id.indexOf("div-gpt-ad") >= 0){
@@ -31,40 +30,29 @@ else {
 			    el.style.transform = 'translate(-50%)';
 			    el.style.bottom = "0px";
 		      el.style.zIndex = "800";
-					//el.style.margin = "10px 0px 0px 0px";
-					//el.style.padding = "10px 0px 0px 0px"
-			    el.style.height = "50px";
-			    el.style.width = "320px";
-					
-					closeButton = document.getElementById("close");
-					closeButton.style.position = 'fixed';
-					closeButton.style.top = "0px";
-					closeButton.style.right = "10px";
-					closeButton.style.zIndex = "1000";
-					closeButton.style.backgroundImage = "url('https://s3.amazonaws.com/script-tags/close.png')";
-					closeButton.style.backgroundSize = 'contain';
-					closeButton.style.backgroundRepeat = 'no-repeat';
-					closeButton.style.width = "18px";
-					closeButton.style.height = "18px";
-					closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false; console.log("closed the ad");};
-
-			    // closeButton = document.getElementById("close");
-			    // closeButton.style.position = 'fixed';
-			    // closeButton.style.top = "0px";
-			    // closeButton.style.right = "5px";
-			    // closeButton.style.zIndex = "1000";
-			    // closeButton.style.backgroundImage = "url('https://s3.amazonaws.com/script-tags/close.png')";
-			    // closeButton.style.backgroundSize = 'contain';
-			    // closeButton.style.backgroundRepeat = 'no-repeat';
-			    // closeButton.style.width = "18px";
-			    // closeButton.style.height = "18px";
-			    // closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false; console.log("closed the ad");};
+			    el.style.height = "60px";
+			    el.style.width = "340px";
+          innerDiv = document.querySelector('[id^="gpt_unit_"]');
+          innerDiv.style.position = "fixed";
+          innerDiv.style.bottom = "0px"
+          innerDiv.style.left = "50%";
+			    innerDiv.style.transform = 'translate(-50%)';
+			    closeButton = document.getElementById("close");
+			    closeButton.style.position = 'fixed';
+			    closeButton.style.top = "-10px";
+			    closeButton.style.right = "-10px";
+			    closeButton.style.zIndex = "1000";
+			    closeButton.style.backgroundImage = "url('https://s3.amazonaws.com/script-tags/close.png')";
+			    closeButton.style.backgroundSize = 'contain';
+			    closeButton.style.backgroundRepeat = 'no-repeat';
+			    closeButton.style.width = "20px";
+			    closeButton.style.height = "20px";
+			    closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode.parentNode); return false;};
 			}
 		}
 	}
 }
 window.onload = updateParentDiv;
-
 
 
 // function updateParentDiv() {
@@ -74,32 +62,151 @@ window.onload = updateParentDiv;
 //
 // if ( window.self != window.top) {
 //     el = window.parent.document.getElementsByName(window.self.name) [0];
+//
+//     // Reference iFrame
+//     el.height = "60px";
+//     el.width = "320px";
+//     // el.style.zIndex = "900";
+//
+//
+//     // Reference iFrame
+//     window.self.document.getElementsByTagName("body")[0].style.margin="10px 0px 0px 0px";
+//     // window.self.document.getElementsByTagName("body")[0].style.padding="0px";
+//
 // }
 // else {
 //     el = document.getElementById("HCM");
 // }
 // 	while (el.parentNode) {
 // 		el = el.parentNode;
+//
 // 		if (el.tagName == 'DIV') {
 // 			tag_id = el.id;
 // 			if( tag_id.indexOf("div-gpt-ad") >= 0){
-// 				el.style.position = 'fixed';
-// 				el.style.left = "50%";
-// 				el.style.transform = 'translate(-50%)';
-// 				el.style.bottom = "0px";
-//         el.style.zIndex = "100";
-// 				closeButton = document.getElementById("close");
-// 				closeButton.style.position = 'fixed';
-// 				closeButton.style.top = "3px";
-// 				closeButton.style.right = "3px";
-// 				closeButton.style.zIndex = "500";
-// 				closeButton.style.backgroundImage = "url('http://www.hcodetestpage.com/close.png')";
-// 				closeButton.style.backgroundSize = 'contain';
-// 				closeButton.style.backgroundRepeat = 'no-repeat';
-// 				closeButton.style.width = "20px";
-// 				closeButton.style.height = "20px";
-// 				closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false; console.log("closed the ad");};
-//       }
+// 			    el.style.position = 'fixed';
+// 			    el.style.left = "50%";
+// 			    el.style.transform = 'translate(-50%)';
+// 			    el.style.bottom = "0px";
+// 		      el.style.zIndex = "800";
+// 					//el.style.margin = "10px 0px 0px 0px";
+// 					//el.style.padding = "10px 0px 0px 0px"
+// 			    el.style.height = "50px";
+// 			    el.style.width = "320px";
+//
+// 					closeButton = document.getElementById("close");
+// 					closeButton.style.position = 'fixed';
+// 					closeButton.style.top = "0px";
+// 					closeButton.style.right = "10px";
+// 					closeButton.style.zIndex = "1000";
+// 					closeButton.style.backgroundImage = "url('https://s3.amazonaws.com/script-tags/close.png')";
+// 					closeButton.style.backgroundSize = 'contain';
+// 					closeButton.style.backgroundRepeat = 'no-repeat';
+// 					closeButton.style.width = "18px";
+// 					closeButton.style.height = "18px";
+// 					closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false; console.log("closed the ad");};
+//
+// 			    // closeButton = document.getElementById("close");
+// 			    // closeButton.style.position = 'fixed';
+// 			    // closeButton.style.top = "0px";
+// 			    // closeButton.style.right = "5px";
+// 			    // closeButton.style.zIndex = "1000";
+// 			    // closeButton.style.backgroundImage = "url('https://s3.amazonaws.com/script-tags/close.png')";
+// 			    // closeButton.style.backgroundSize = 'contain';
+// 			    // closeButton.style.backgroundRepeat = 'no-repeat';
+// 			    // closeButton.style.width = "18px";
+// 			    // closeButton.style.height = "18px";
+// 			    // closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false; console.log("closed the ad");};
+// 			}
+// 		}
+// 	}
+// }
+// window.onload = updateParentDiv;
+//
+//
+//
+// // function updateParentDiv() {
+// // 	var tag_id;
+// // 	var el;
+// // 	var closeButton;
+// //
+// // if ( window.self != window.top) {
+// //     el = window.parent.document.getElementsByName(window.self.name) [0];
+// // }
+// // else {
+// //     el = document.getElementById("HCM");
+// // }
+// // 	while (el.parentNode) {
+// // 		el = el.parentNode;
+// // 		if (el.tagName == 'DIV') {
+// // 			tag_id = el.id;
+// // 			if( tag_id.indexOf("div-gpt-ad") >= 0){
+// // 				el.style.position = 'fixed';
+// // 				el.style.left = "50%";
+// // 				el.style.transform = 'translate(-50%)';
+// // 				el.style.bottom = "0px";
+// //         el.style.zIndex = "100";
+// // 				closeButton = document.getElementById("close");
+// // 				closeButton.style.position = 'fixed';
+// // 				closeButton.style.top = "3px";
+// // 				closeButton.style.right = "3px";
+// // 				closeButton.style.zIndex = "500";
+// // 				closeButton.style.backgroundImage = "url('http://www.hcodetestpage.com/close.png')";
+// // 				closeButton.style.backgroundSize = 'contain';
+// // 				closeButton.style.backgroundRepeat = 'no-repeat';
+// // 				closeButton.style.width = "20px";
+// // 				closeButton.style.height = "20px";
+// // 				closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false; console.log("closed the ad");};
+// //       }
+// // 		}
+// // 	}
+// // }
+// // window.onload = updateParentDiv;
+//
+//
+//
+//
+//
+//
+// function updateParentDiv() {
+// 	var tag_id;
+// 	var el;
+// 	var closeButton;
+//
+// if ( window.self != window.top) {
+//     el = window.parent.document.getElementsByName(window.self.name) [0];
+//     el.height = "60px";
+//     el.width = "320px";
+//     window.self.document.getElementsByTagName("body")[0].style.margin="10px 0px 0px 0px";
+//
+// }
+// else {
+//     el = document.getElementById("HCM");
+// }
+// 	while (el.parentNode) {
+// 		el = el.parentNode;
+//
+// 		if (el.tagName == 'DIV') {
+// 			tag_id = el.id;
+// 			if( tag_id.indexOf("div-gpt-ad") >= 0){
+// 			    el.style.position = 'fixed';
+// 			    el.style.left = "50%";
+// 			    el.style.transform = 'translate(-50%)';
+// 			    el.style.bottom = "0px";
+// 		      el.style.zIndex = "800";
+// 			    el.style.height = "50px";
+// 			    el.style.width = "320px";
+// 					closeButton = document.getElementById("close");
+// 					closeButton.style.position = 'fixed';
+// 					closeButton.style.top = "0px";
+// 					closeButton.style.right = "10px";
+// 					closeButton.style.zIndex = "1000";
+// 					closeButton.style.backgroundImage = "url('https://s3.amazonaws.com/script-tags/close.png')";
+// 					closeButton.style.backgroundSize = 'contain';
+// 					closeButton.style.backgroundRepeat = 'no-repeat';
+// 					closeButton.style.width = "18px";
+// 					closeButton.style.height = "18px";
+// 					closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode.parentNode); return false;};
+// 			}
 // 		}
 // 	}
 // }

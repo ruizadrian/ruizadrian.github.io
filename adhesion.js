48e7850ba070b60df3b45650dcb1d488907f45e4
Adrian Ruiz <adrianruiz@Adrians-MacBook-Air-2.local>
2017-03-14 23:51:04 -0700

    test pages

--- adhesion.js
+++ adhesion.js
@@ -23,7 +23,7 @@ else {
 		el = el.parentNode;
 		closeButton = document.getElementById("close");
 		closeButton.style.position = 'fixed';
-		closeButton.style.top = "0px";
+		closeButton.style.top = "-10px";
 		closeButton.style.right = "5px";
 		closeButton.style.zIndex = "1000";
 		closeButton.style.backgroundImage = "url('https://s3.amazonaws.com/script-tags/close.png')";

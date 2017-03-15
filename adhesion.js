dc31711e72649d86c5d8b627ccab1c1cd16e19b7
Adrian Ruiz <adrianruiz@Adrians-MacBook-Air-2.local>
2017-03-14 22:53:48 -0700

    test pages

--- adhesion.js
+++ adhesion.js
@@ -2,9 +2,19 @@ function updateParentDiv() {
 	var tag_id;
 	var el;
 	var closeButton;
-
 if ( window.self != window.top) {
     el = window.parent.document.getElementsByName(window.self.name) [0];
+
+    // Reference iFrame
+    // el.height = "60px";
+    // el.width = "340px";
+    // el.style.zIndex = "900";
+
+
+    // Reference iFrame
+    // window.self.document.getElementsByTagName("body")[0].style.margin="10px 10px 0px 10px";
+    // window.self.document.getElementsByTagName("body")[0].style.padding="0px";
+
 }
 else {
     el = document.getElementById("HCM");
@@ -14,24 +24,68 @@ else {
 		if (el.tagName == 'DIV') {
 			tag_id = el.id;
 			if( tag_id.indexOf("div-gpt-ad") >= 0){
-				el.style.position = 'fixed';
-				el.style.left = "50%";
-				el.style.transform = 'translate(-50%)';
-				el.style.bottom = "0px";
-        el.style.zIndex = "100";
-				closeButton = document.getElementById("close");
-				closeButton.style.position = 'fixed';
-				closeButton.style.top = "3px";
-				closeButton.style.right = "3px";
-				closeButton.style.zIndex = "500";
-				closeButton.style.backgroundImage = "url('http://www.hcodetestpage.com/close.png')";
-				closeButton.style.backgroundSize = 'contain';
-				closeButton.style.backgroundRepeat = 'no-repeat';
-				closeButton.style.width = "20px";
-				closeButton.style.height = "20px";
-				closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false; console.log("closed the ad");};
-      }
+			    el.style.position = 'fixed';
+			    el.style.left = "50%";
+			    el.style.transform = 'translate(-50%)';
+			    el.style.bottom = "0px";
+		      el.style.zIndex = "800";
+			    // el.style.height = "60px";
+			    // el.style.width = "340px";
+
+
+			    closeButton = document.getElementById("close");
+			    closeButton.style.position = 'fixed';
+			    closeButton.style.top = "0px";
+			    closeButton.style.right = "0px";
+			    closeButton.style.zIndex = "1000";
+			    closeButton.style.backgroundImage = "url('https://s3.amazonaws.com/script-tags/close.png')";
+			    closeButton.style.backgroundSize = 'contain';
+			    closeButton.style.backgroundRepeat = 'no-repeat';
+			    closeButton.style.width = "20px";
+			    closeButton.style.height = "20px";
+			    closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false; console.log("closed the ad");};
+			}
 		}
 	}
 }
 window.onload = updateParentDiv;
+
+
+
+// function updateParentDiv() {
+// 	var tag_id;
+// 	var el;
+// 	var closeButton;
+//
+// if ( window.self != window.top) {
+//     el = window.parent.document.getElementsByName(window.self.name) [0];
+// }
+// else {
+//     el = document.getElementById("HCM");
+// }
+// 	while (el.parentNode) {
+// 		el = el.parentNode;
+// 		if (el.tagName == 'DIV') {
+// 			tag_id = el.id;
+// 			if( tag_id.indexOf("div-gpt-ad") >= 0){
+// 				el.style.position = 'fixed';
+// 				el.style.left = "50%";
+// 				el.style.transform = 'translate(-50%)';
+// 				el.style.bottom = "0px";
+//         el.style.zIndex = "100";
+// 				closeButton = document.getElementById("close");
+// 				closeButton.style.position = 'fixed';
+// 				closeButton.style.top = "3px";
+// 				closeButton.style.right = "3px";
+// 				closeButton.style.zIndex = "500";
+// 				closeButton.style.backgroundImage = "url('http://www.hcodetestpage.com/close.png')";
+// 				closeButton.style.backgroundSize = 'contain';
+// 				closeButton.style.backgroundRepeat = 'no-repeat';
+// 				closeButton.style.width = "20px";
+// 				closeButton.style.height = "20px";
+// 				closeButton.onclick = function() {this.parentNode.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode.parentNode); return false; console.log("closed the ad");};
+//       }
+// 		}
+// 	}
+// }
+// window.onload = updateParentDiv;

e3441bfb52cf12256c6e437214d497743f19f6ff
ruizadrian <adrianr04@gmail.com>
2025-01-01 12:35:28 -0800

    Rummikub Timer

--- gruuv.js
+++
@@ -1,9 +0,0 @@
-function autoPlay() {
-	window.document.querySelector('div[style*="https://cdn.adtags.mobi/shared/img/play.png"]').click();
-}
-
-function playTimer(){
-	setTimeout(function() { autoplay(); }, 10000);
-}
-
-window.onload = playTimer;
